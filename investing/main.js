e411125a265a044df7116326fdcba7b124a6cdf5
Bruno Mattos <brunoandremattos@gmail.com>
2018-08-25 00:15:20 -0300

    trying to style checkbox

--- investing/main.js
+++ investing/main.js
@@ -1,10 +1,9 @@
 document.getElementById('people-hyped').addEventListener('click', function() {
     document.getElementById('result').innerHTML = "Sell";
-    this.children[0].checked = true;
+    this.children[1].checked = true;
 });
 
 document.getElementById('people-fearful').addEventListener('click', function() {
     document.getElementById('result').innerHTML = "Buy";
-    this.children[0].checked = true;
+    this.children[1].checked = true;
 });
-
