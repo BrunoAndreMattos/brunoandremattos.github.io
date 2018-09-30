1e8189f55272293834db3a072fa86f5b94886470
Bruno Mattos <brunoandremattos@gmail.com>
2018-09-29 22:40:24 -0300

    chore: changing files around

--- playing-around/main.js
+++
@@ -1,5 +0,0 @@
-Array.from(document.getElementsByClassName('item-menu')).forEach(function(e) {
-    e.addEventListener('click', function () {
-        document.getElementById('check-burger').click();
-    });
-});
