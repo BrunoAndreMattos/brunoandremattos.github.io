b756599ed15f6f789aedf9a2cfa01e9930168b78
Bruno Mattos <brunoandremattos@gmail.com>
2018-10-11 03:21:10 -0300

    feat: hamburger better adapted to background

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ Array.from(document.getElementsByClassName('item-menu')).forEach((e) => {
 
 // Changes the hambuger color after clicking according to the background
 document.getElementById('check-burger').addEventListener('click', function() {
-    const header = document.getElementById("header");
-    if(this.checked == false && window.scrollY < (header.offsetTop + header.offsetHeight)) {
-        Array.from(document.getElementsByTagName('span')).forEach((e) => {
-            e.style.backgroundColor = '#f4f4f4';
-        });
+    const header = document.getElementsByTagName('header')[0];
+    const projects = document.getElementById('projects');
+
+    if(this.checked == false) {
+        if ((window.scrollY < (header.offsetTop + header.offsetHeight))
+         || (window.scrollY > (projects.offsetTop + projects.offsetHeight))) {
+
+            Array.from(document.getElementsByTagName('span')).forEach((e) => {
+                e.style.backgroundColor = '#f4f4f4';
+            });
+        }
     } else {
         Array.from(document.getElementsByTagName('span')).forEach((e) => {
             e.style.backgroundColor = '#233D4D';
@@ -21,18 +27,53 @@ document.getElementById('check-burger').addEventListener('click', function() {
 
 // Changes the hambuger color according to the background
 window.addEventListener('scroll', () => {
-    Array.from(document.getElementsByTagName("header")).forEach((e) => {
-        if (
-            window.scrollY < (e.offsetTop + e.offsetHeight) &&
-            document.getElementById("check-burger").checked == false
-            ) {
-            Array.from(document.getElementsByTagName("span")).forEach((e) => {
+    const header = document.getElementsByTagName('header')[0];
+    const projects = document.getElementById('projects');
+
+    if(document.getElementById('check-burger').checked == false) {
+        if ((window.scrollY < (header.offsetTop + header.offsetHeight))
+         || (window.scrollY > (projects.offsetTop + projects.offsetHeight))) {
+
+            Array.from(document.getElementsByTagName('span')).forEach((e) => {
                 e.style.backgroundColor = '#f4f4f4';
             });
         } else {
-            Array.from(document.getElementsByTagName("span")).forEach((e) => {
+            Array.from(document.getElementsByTagName('span')).forEach((e) => {
                 e.style.backgroundColor = '#233D4D';
             });
         }
+    }
+});
+
+// Smooth scrolling
+function myScroll(elem) {
+    window.scrollTo({
+        'behavior': 'smooth',
+        'left': 0,
+        'top': elem.offsetTop
     });
+}
+
+document.querySelector('#m-header').addEventListener('click', () => {
+    const elem = document.querySelector('body');
+
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+
+document.querySelector('#m-projects').addEventListener('click', () => {
+    const elem = document.querySelector('#projects');
+
+    myScroll(elem);
+});
+
+document.querySelector('#m-what-i-know').addEventListener('click', () => {
+    const elem = document.querySelector('#what-i-know');
+
+    myScroll(elem);
+});
+
+document.querySelector('#m-contact').addEventListener('click', () => {
+    const elem = document.querySelector('#contact');
+
+    myScroll(elem);
 });
