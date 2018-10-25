9faf81fa6e5c6a96e1df1ec82703a2891857029c
Bruno Mattos <brunoandremattos@gmail.com>
2018-10-25 14:10:25 -0300

    chore

--- sites/game/main.js
+++ sites/game/main.js
@@ -1,7 +1,8 @@
 let timer = 0;
 const button = document.querySelector('p');
 
+
 button.addEventListener('click', () => {
     timer++;
-    button.textContent() = `click me ${timer}!`;
+    button.textContent = `click me ${timer}!`;
 });
