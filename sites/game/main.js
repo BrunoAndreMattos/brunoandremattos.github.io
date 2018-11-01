0c457d66a2ea142c71172d701d2b93448c298e7f
Bruno Mattos <brunoandremattos@gmail.com>
2018-11-01 14:52:25 -0300

    feat: add progress bar for mining

--- sites/game/main.js
+++ sites/game/main.js
@@ -1,10 +1,33 @@
-let coins = 0;
+const display = document.getElementById('display');
+const progress = document.getElementById('progress');
 const button = document.getElementById('button');
+let coins = 0;
+let mining = false;
+let counter = 0;
+let miningSpeed = 10000;
+
 
 
 button.addEventListener('click', () => {
-    coins++;
-    document.getElementById('display').textContent = `You have ${coins} coins!`;
-});
+    document.getElementById('display').textContent = `You have ${coins} Bitcoins!`;
+    if (mining === false) {
+        mining = true;
+        
+        let a = setInterval(() => {
+            counter++;
+            progress.textContent += '░';
+            
+            if(counter === 10) {
+                coins++;
+                clearInterval(a);
+                counter = 0;
+                mining = false;
+                document.getElementById('display').textContent = `You have ${coins} Bitcoins!`;
+            }
+        }, miningSpeed);
+        
+    } else {
+        console.log('mining in progress');
+    }
 
-// setInterval(() => {button.click();}, 500);
+});
