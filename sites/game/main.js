e6528baf6b2506b8db143abd89bc79e25f7b2ad3
Bruno Mattos <brunoandremattos@gmail.com>
2018-11-05 17:13:44 -0200

    feat: add coinsmarketcap API

--- sites/game/main.js
+++ sites/game/main.js
@@ -1,11 +1,9 @@
 const display = document.getElementById('display');
 const progress = document.getElementById('progress');
 const button = document.getElementById('button');
+const usdSec = document.getElementById('usd');
 const totalLoad = 10;
-
-// types:
-// Cents
-
+                          
 class Computer {
     constructor(isMining, coins, miningSpeed) {
         this.isMining = isMining;
@@ -19,7 +17,7 @@ class Computer {
     }
     
     displayCoins() {
-        document.getElementById('display').textContent = `You have ${this.getCoins()} Bitcoins in this machine!`;
+        document.getElementById('display').textContent = `You have ${this.getCoins()} Satoshis in this machine!`;
     }
 
     getMiningSpeed() {
@@ -73,3 +71,21 @@ button.addEventListener('click', () => {
     myComp.mine();
     myComp.displayCoins();
 });
+
+var json = new XMLHttpRequest(); // start a new variable to store the JSON in
+json.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) { // if HTTP header 200 - ok
+    var object = JSON.parse(this.responseText); // set the variable 'object' to whatever we get back, in our case it is an array of 10 different arrays
+
+    object.forEach(function(currency) { // for each of those arrays, split it into chunks called 'currency'
+        console.log(currency.name);
+        usdSec.textContent = currency.name + " is currently worth $" + currency.price_usd + " USD"; // get the array keys from the API
+    });
+  }
+};
+json.open(
+  "GET", // method
+  "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=10", // url
+  true // async
+); // initialise the request
+json.send(); //send request
