798720792edd731a4ac977bc41a44d5155603418
Bruno Mattos <brunoandremattos@gmail.com>
2018-11-06 18:59:37 -0200

    commit

--- sites/game/main.js
+++ sites/game/main.js
@@ -118,15 +118,15 @@ button.addEventListener('click', () => {
 
 var json = new XMLHttpRequest(); // start a new variable to store the JSON in
 json.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) { // if HTTP header 200 - ok
-    var object = JSON.parse(this.responseText); // set the variable 'object' to whatever we get back, in our case it is an array of 10 different arrays
+    if (this.readyState == 4 && this.status == 200) { // if HTTP header 200 - ok
+        var object = JSON.parse(this.responseText); // set the variable 'object' to whatever we get back, in our case it is an array of 10 different arrays
 
-    object.forEach(function(currency) { // for each of those arrays, split it into chunks called 'currency'
-        if(currency.name === 'Bitcoin') {
-            let dolar = (currency.price_usd * defaultComp.getBitcoins()).toFixed(4);
+        object.forEach(function(currency) { // for each of those arrays, split it into chunks called 'currency'
+            if(currency.name === 'Bitcoin') {
+                let dolar = (currency.price_usd * defaultComp.getBitcoins()).toFixed(4);
 
-            usdSec.textContent =  `You Bitcoin in Dollars: ${dolar}\$`; // get the array keys from the API
-        }
-    });
-  }
+                usdSec.textContent =  `You Bitcoin in Dollars: ${dolar}\$`; // get the array keys from the API
+            }
+        });
+    }
 };
