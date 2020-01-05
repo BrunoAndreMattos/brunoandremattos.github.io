38cdcc777d23b803470f3c10c37ea32b29f9816f
Bruno Mattos <brunoandremattos@gmail.com>
2020-01-05 14:27:04 -0300

    refactored and restyled the whole website

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// Closes the hamburger after clicked on a menu item
-Array.from(document.getElementsByClassName('item-menu')).forEach((e) => {
-    e.addEventListener('click', () => {
-        document.getElementById('check-burger').click();
-    });
-});
-
 // Smooth scrolling
 function myScroll(elem) {
     window.scrollTo({
@@ -14,20 +7,14 @@ function myScroll(elem) {
     });
 }
 
-document.querySelector('#m-header').addEventListener('click', () => {
-    const elem = document.querySelector('body');
-
-    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-});
-
-document.querySelector('#m-projects').addEventListener('click', () => {
-    const elem = document.querySelector('main');
+document.querySelector('#m-what-i-know').addEventListener('click', () => {
+    const elem = document.querySelector('#experience');
 
     myScroll(elem);
 });
 
-document.querySelector('#m-what-i-know').addEventListener('click', () => {
-    const elem = document.querySelector('#what-i-know');
+document.querySelector('#m-projects').addEventListener('click', () => {
+    const elem = document.querySelector('#projects');
 
     myScroll(elem);
 });
@@ -36,29 +23,4 @@ document.querySelector('#m-contact').addEventListener('click', () => {
     const elem = document.querySelector('#contact');
 
     myScroll(elem);
-});
-
-// Close the menu by clicking out
-document.getElementById('click-aux').addEventListener('click', () => {
-    const menu = document.getElementById('check-burger');
-
-    if(menu.checked == true) {
-        menu.click();
-    }
-});
-
-document.querySelector('main').addEventListener('click', () => {
-    const menu = document.getElementById('check-burger');
-
-    if(menu.checked == true) {
-        menu.click();
-    }
-});
-
-document.querySelector('footer').addEventListener('click', () => {
-    const menu = document.getElementById('check-burger');
-
-    if(menu.checked == true) {
-        menu.click();
-    }
 });
